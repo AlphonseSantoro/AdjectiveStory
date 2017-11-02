49ad7b9f26ce675e468017c9f45f6184882a4049
AlphonseSantoro <kslagnes@mimer.no>
2017-11-02 21:12:23 +0100

    Update StoryCreator.java

--- StoryCreator.java
+++ StoryCreator.java
@@ -3,9 +3,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.awt.Desktop;
 import java.net.URI;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.io.File;
 /** 
  *  Class for creating an adjective story.
